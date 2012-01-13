916e61590f87bf62048384a8e9d33731f1f1021e
bio7 <marcel.au@web.de>
2012-01-13 10:57:27 +0100

    Example for JSS

---
+++ JSS/Example.java
@@ -0,0 +1,6 @@
+public void setup(GL gl,GLU glu,GLUT glut){
+// Please enter your Java OpenGL setup code here
+}
+public void ecomain(GL gl,GLU glu,GLUT glut){
+// Please enter your Java OpenGL code here
+}
